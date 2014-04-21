1503109fb46e0185691297d8496913435b3dc12d
Christopher Cartland <c.cartland@gmail.com>
2014-04-21 00:56:29 -0700

    Removing old comment.

--- imagic/example/example.go
+++ imagic/example/example.go
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// This example demonstrates decoding a JPEG image and examining its pixels.
 package main
 
 import (
