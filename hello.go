f13836d3204e861380a232f5911ee7e5f6fe421b
Chris Cartland <c.cartland@gmail.com>
2014-04-20 17:21:58 -0700

    Use fib in hello.go.

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "github.com/cartland/hello-go/fib"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+  m := fib.NewMemoizer()
+  for i := 0; i < 10; i++ {
+    fmt.Printf("f.Fib($v) $v", i, f.Fib(i))
+  }
 }
