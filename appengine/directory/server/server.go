10379262253162ca607d448164bbdb86c090b212
cartland <c.cartland@gmail.com>
2015-02-05 23:38:01 -0800

    Stub directory parses listing JSON and returns it.

---
+++ appengine/directory/server/server.go
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2015 Chris Cartland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package server
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"encoding/json"
+	"net/http"
+
+	"github.com/mjibson/appstats"
+)
+
+type listing struct {
+	Name       string `json:"name"`       // Resource name
+	Location   string `json:"location"`   // URL
+	Expiration int64  `json:"expiration"` // UNIX time in seconds
+}
+
+func init() {
+	http.Handle("/", appstats.NewHandler(register))
+}
+
+func register(c appengine.Context, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case "GET":
+		getDirectory(c, w, r)
+		return
+	case "PUT":
+		putListing(c, w, r)
+	default:
+		c.Errorf("Method %v not expected.", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+	}
+}
+
+func getDirectory(c appengine.Context, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	q := datastore.NewQuery("listing")
+	var listings []listing
+	_, err := q.GetAll(c, &listings)
+	if err != nil {
+		c.Errorf("getDirectory GetAll query failed %v", err)
+	}
+	b, err := json.Marshal(listings)
+	if err != nil {
+		c.Errorf("getDirectory Marshal failed %v", err)
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		c.Errorf("getDirectory %v", err)
+	}
+}
+
+func putListing(c appengine.Context, w http.ResponseWriter, r *http.Request) {
+	newListing := new(listing)
+	requestData := make([]byte, 100)
+	n, err := r.Body.Read(requestData)
+	if err != nil {
+		c.Errorf("addListing read body %v", err)
+	}
+	err = json.Unmarshal(requestData[:n], &newListing)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		c.Errorf("addListing Unmarshal %v", err)
+	}
+	b, err := json.Marshal(newListing)
+	if err != nil {
+		c.Errorf("addListing Marshal %v", err)
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		c.Errorf("addListing Write %v", err)
+	}
+}
